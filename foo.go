335813590929fd571008ed2fc3a83e5468c4f2fa
Pawel Palucki <pawel.palucki@intel.com>
2016-05-23 19:52:56 +0200

    WIP

--- foo.go
+++ foo.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 
 	"github.com/ppalucki/bar/subbar"
+	"k8s.io/kubernetes/pkg/api/resource"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
 func main() {
 
+	println(resource.Scale(1))
+	println(client.ConfigMapResourceName)
 	fmt.Println(subbar.Subbar)
 }
